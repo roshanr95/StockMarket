89465880cbfb667bf44bf2d28b706db285f02c3b
Roshan <thisisroshansmail@gmail.com>
2014-11-23 02:44:14 +0530

    TransactionHistory

--- Schema.sql
+++ Schema.sql
@@ -41,7 +41,7 @@ create table stock_history
 create table ownership
 	(userid		varchar(20),
 	 ticker_symbol varchar(10) not null, 
-	 invest_type  varchar(2) check(ticker_symbol in ('B','S','MF')),
+	 invest_type  varchar(2) check(invest_type in ('B','S','MF')),
 	 quantity  numeric(12,0) check (quantity >= 0),
 	 primary key (userid,ticker_symbol,invest_type),
 	 foreign key (userid) references users on delete set null,
@@ -77,7 +77,7 @@ create table curr_transact
 	(trans_id 	numeric(20,0) not null,
 	 userid  varchar(20) not null,
 	 ticker_symbol varchar(10),
-	 invest_type varchar(2) check(ticker_symbol in ('B','S','MF')),
+	 invest_type varchar(2) check(invest_type in ('B','S','MF')),
 	 trans_type varchar(1) check(trans_type in ('B','S')),
 	 price numeric(10,2),
 	 quantity numeric(12,0),
@@ -91,12 +91,12 @@ create table transact_history
 	(trans_id 	numeric(20,0) not null,
 	 userid  varchar(20) not null,
 	 ticker_symbol varchar(10),
-	 invest_type varchar(2) check(ticker_symbol in ('B','S','MF')),
+	 invest_type varchar(2) check(invest_type in ('B','S','MF')),
 	 trans_type varchar(1) check(trans_type in ('B','S')),
 	 price numeric(10,2),
 	 quantity numeric(12,0),
 	 time timestamp,
-	 success_trans varchar(1) check (success_trans in ('y','n')),
+	 success_trans varchar(1) check (success_trans in ('Y','N')),
 	 primary key (trans_id),
 	 foreign key (userid) references users
 		on delete set null,
@@ -104,7 +104,7 @@ create table transact_history
 	);
  create table game_params
  	( game_param varchar(20),
- 	  value numeric(10,3),
+ 	  value varchar(20),
  	  primary key (game_param)
  	);
  create table fd_rates
@@ -119,3 +119,11 @@ create table transact_history
  CREATE INDEX userid_fdhist ON fd_history (userid);
  CREATE INDEX day_fdhist ON fd_history (day_of_issue);
  insert into users values('admin','admin','99999999999','9999999999','admin@gmail.com','admin');
+ insert into users values('sanket','sanket','100000','100000','sanket1729@gmail.com','sanket');
+ insert into users values('karan','karan','100000','100000','sanket1729@gmail.com','karan');
+ insert into users values('rosh','rosh','100000','100000','sanket1729@gmail.com','rosh');
+ insert into company values('ABC','ABC inc.','120.2');
+ insert into company values('XYZ','XYZ inc.','220.2');
+ insert into company values('DEF','DEF inc.','20.2');
+ insert into game_params values('fd_no','10000');
+ insert into game_params values('trans_no','100000');
--- src/PastTransactions.java
+++ src/PastTransactions.java
@@ -1,11 +1,18 @@
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 //This will handle all requests from OrderBook and TransactionHistory
 
@@ -34,7 +41,94 @@ public class PastTransactions extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String user = (String) session.getAttribute("username");
+		
+		String startDate = request.getParameter("startdate");
+		String endDate = request.getParameter("enddate");
+		
+		Date start, end;
+		
+		try {
+			start = Date.valueOf(startDate);
+			end = Date.valueOf(endDate);
+		} catch (IllegalArgumentException e) {
+			response.sendRedirect("TransactionHistory.jsp?res=format");
+			return;
+		}
+		
+		if(start.compareTo(end) >= 0) {
+			response.sendRedirect("TransactionHistory.jsp?res=range");
+			return;
+		}
+		
+		Timestamp startt = new Timestamp(start.getTime());
+		Timestamp endt = new Timestamp(end.getTime());
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement("select * from transact_history where userid = ? and invest_type='S' and time between ? and ?;");
+			stmt.setString(1, user);
+			stmt.setTimestamp(2, startt);
+			stmt.setTimestamp(3, endt);
+			ResultSet rs = stmt.executeQuery();
+			String res="";
+			
+			while(rs.next()) {
+				String r = String.valueOf(rs.getInt(1)) + "," + rs.getString(3) + "," + 
+							rs.getString(5) + "," + String.valueOf(rs.getFloat(6)) + "," + String.valueOf(rs.getInt(7)) + "," +
+							rs.getTimestamp(8) + "," + rs.getString(9);
+				res = res + r + ";";
+			}
+//			System.out.println(res);
+			session.setAttribute("stock", res);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement("select * from transact_history where userid = ? and invest_type='MF' and time between ? and ?;");
+			stmt.setString(1, user);
+			stmt.setTimestamp(2, startt);
+			stmt.setTimestamp(3, endt);
+			ResultSet rs = stmt.executeQuery();
+			String res="";
+			
+			while(rs.next()) {
+				String r = String.valueOf(rs.getInt(1)) + "," + rs.getString(3) + "," + 
+							rs.getString(5) + "," + String.valueOf(rs.getFloat(6)) + "," + String.valueOf(rs.getInt(7)) + "," +
+							rs.getTimestamp(8) + "," + rs.getString(9);
+				res = res + r + ";";
+			}
+//			System.out.println(res);
+			session.setAttribute("mf", res);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement("select * from transact_history where userid = ? and invest_type='B' and time between ? and ?;");
+			stmt.setString(1, user);
+			stmt.setTimestamp(2, startt);
+			stmt.setTimestamp(3, endt);
+			ResultSet rs = stmt.executeQuery();
+			String res="";
+			
+			while(rs.next()) {
+				String r = String.valueOf(rs.getInt(1)) + "," + rs.getString(3) + "," + 
+							rs.getString(5) + "," + String.valueOf(rs.getFloat(6)) + "," + String.valueOf(rs.getInt(7)) + "," +
+							rs.getTimestamp(8) + "," + rs.getString(9);
+				res = res + r + ";";
+			}
+//			System.out.println(res);
+			session.setAttribute("bond", res);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		response.sendRedirect("TransactionHistory.jsp?res=success");
 	}
 
 	/**
