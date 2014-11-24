7c46c19a2f895157878cc92c0c61c5e1cc38782f
sanket <sanket1729@gmail.com>
2014-11-24 09:54:29 +0530

    working state

--- WebContent/Transact.jsp
+++ WebContent/Transact.jsp
@@ -16,7 +16,11 @@
 	</div>
 
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		<% if(session.getAttribute("username") != null) out.println("<p class=\"navbar-text\"><strong>Hello "+session.getAttribute("username")+"</strong></p>");%>
+		<%
+			if (session.getAttribute("username") != null)
+				out.println("<p class=\"navbar-text\"><strong>Hello "
+						+ session.getAttribute("username") + "</strong></p>");
+		%>
 		<ul class="nav navbar-nav">
 			<li><a href="Profile.jsp">Profile</a>
 			</li>
@@ -27,7 +31,11 @@
 			</li>
 			<li><a href="TransactionHistory.jsp">Transaction History</a>
 			</li>
-			<li style="height: 50px; padding: 15px;"><form style="height:20px" method="post" action="PastTransactions"><button style="padding:0px" class="btn btn-link" type=submit>Order Book</button></form></li>
+			<li style="height: 50px; padding: 15px;"><form
+					style="height: 20px" method="post" action="PastTransactions">
+					<button style="padding: 0px" class="btn btn-link" type=submit>Order
+						Book</button>
+				</form></li>
 			<li><a href="Market.jsp">Market Statistics</a>
 			</li>
 		</ul>
@@ -48,64 +56,106 @@
 
 	<legend>Transact</legend>
 	<%
-		if (request.getParameter("res") == null) ;
-		else if (request.getParameter("res").equals("funds")) out.println("<div class=\"alert alert-warning\" role=\"alert\">You don't have sufficent funds!.</div>");
-		else if (request.getParameter("res").equals("stocks")) out.println("<div class=\"alert alert-warning\" role=\"alert\">You don't posses the required amount of shares!</div>");
+		if (request.getParameter("res") == null)
+			;
+		else if (request.getParameter("res").equals("funds"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You don't have sufficent funds!.</div>");
+		else if (request.getParameter("res").equals("stocks"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You don't posses the required amount of shares!</div>");
+		else if (request.getParameter("res").equals("invalid_id"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You have entered Invalid fd_id</div>");
+		else if (request.getParameter("res").equals("stock_buy"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You stock has been purchased. Check Transaction History for details</div>");
+		else if (request.getParameter("res").equals("stock_sell"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You stock has been sold. Check Transaction History for details</div>");
+		else if (request.getParameter("res").equals("fd_buy"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You FD has been purchased. Check OrderBook for details</div>");
+		else if (request.getParameter("res").equals("fd_broken"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You FD has been broken. Check OrderBook for details</div>");
+		else if (request.getParameter("res").equals("trans_buy"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You  buy Transaction is posted. Check OrderBook for details</div>");
+		else if (request.getParameter("res").equals("trans_sell"))
+			out.println("<div class=\"alert alert-warning\" role=\"alert\">You sell Transaction is posted. Check OrderBook for details</div>");
 	%>
 	<br>
 	<b>Stock investments</b>
-	<form class="form-inline" role="form" action="NewTransaction" method ="post">
+	<form class="form-inline" role="form" action="NewTransaction"
+		method="post">
 		<br>Choose the company where you wish to invest<br> <select
 			class="form-control" name="stock_company">
 			<% if(session.getAttribute("username")!=null) out.print(session.getAttribute("companiesstock")); %>
 		</select> <select class="form-control" name="stock_type">
 			<option>buy</option>
 			<option>sell</option>
-		</select> <br>Choose the quantity you wish to invest<br> <input
-			type="text" name="stock_quantity">
+			<option>buy_offer</option>
+			<option>sell_offer</option>
+		</select> <br>Choose the quantity you wish to buy/sell<br> <input
+			type="text" name="stock_quantity"> <br>Choose the price
+		at which you wish to buy/sell<br> <input type="text"
+			name="stock_price">
 		<button type="submit" class="btn btn-default">Submit</button>
 		<input type="hidden" name="classify" value="stock">
 	</form>
 
 	<br>
 	<b>Mutual Funds</b>
-	<form class="form-inline" role="form" action="NewTransaction" method ="post">
+	<form class="form-inline" role="form" action="NewTransaction"
+		method="post">
 		<br>Choose the company where you wish to invest<br> <select
 			class="form-control" name="mf_company">
 			<% if(session.getAttribute("username")!=null) out.print(session.getAttribute("companiesmf")); %>
 		</select> <select class="form-control" name="mf_type">
 			<option>buy</option>
 			<option>sell</option>
-		</select> <br>Choose the quantity you wish to invest<br> <input
-			type="text" name="mf_quantity">
+			<option>buy_offer</option>
+			<option>sell_offer</option>
+		</select> <br>Choose the quantity you wish to buy/sell<br> <input
+			type="text" name="mf_quantity"> <br>Choose the price at
+		which you wish to buy/sell<br> <input type="text" name="mf_price">
 		<button type="submit" class="btn btn-default">Submit</button>
 		<input type="hidden" name="classify" value="mf">
 	</form>
 
 	<br>
 	<b>Bonds</b>
-	<form class="form-inline" role="form" action="NewTransaction" method ="post">
+	<form class="form-inline" role="form" action="NewTransaction"
+		method="post">
 		<br>Choose the company where you wish to invest<br> <select
-			class="form-control" name="bonds_type">
-			<option>buy</option>
-			<option>sell</option>
-		</select> <select class="form-control" name="bonds_company">
-		</select> <br>Choose the quantity you wish to invest<br> <input
-			type="text" name="bonds_quantity">
-		<button type="submit" class="btn btn-default">Submit</button>
-		<input type="hidden" name="classify" value="bonds">
+			class="form-control" name="bonds_company">
+			<%
+				if (session.getAttribute("username") != null)
+					out.print(session.getAttribute("companiesstock"));
+			%>
+			</select>
+			<select class="form-control" name="bonds_type">
+				<option>buy</option>
+				<option>sell</option>
+				<option>buy_offer</option>
+			    <option>sell_offer</option>
+		</select>
+			<br>Choose the quantity you wish to buy/sell
+			<br>
+			<input type="text" name="bonds_quantity">
+			<br>Choose the price at which you wish to buy/sell
+			<br>
+			<input type="text" name="bonds_price">
+			<button type="submit" class="btn btn-default">Submit</button>
+			<input type="hidden" name="classify" value="bonds">
 	</form>
 
 
 	<br>
 	<b>Fixed deposit</b>
-	<form class="form-inline" role="form" action="NewTransaction" method ="post">
+	<form class="form-inline" role="form" action="NewTransaction"
+		method="post">
 		<select class="form-control" name="fd_type">
-			<option>Buy FD</option>
-			<option>Break FD</option>
+			<option>Buy_FD</option>
+			<option>Break_FD</option>
 		</select> <br>Enter your FD ID if you wish to break it<br> <input
 			type="text" name="break_fd"> <br>Choose the investment
 		period<br> <select class="form-control" name="fd_period">
+			<option>1 year</option>
+			<option>6 mons</option>
 		</select> <br>Choose the amount you wish to invest<br> <input
 			type="text" name="fd_amount">
 		<button type="submit" class="btn btn-default">Submit</button>
--- src/NewTransaction.java
+++ src/NewTransaction.java
@@ -5,12 +5,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.postgresql.util.PGInterval;
 
 //Handles all Transact operations
 
@@ -55,20 +58,23 @@ public class NewTransaction extends HttpServlet {
 		String stock_quantity=request.getParameter("stock_quantity");
 		String mf_quantity=request.getParameter("mf_quantity");
 		String bonds_quantity=request.getParameter("bonds_quantity");
-		String fd_amount=request.getParameter("fd_aomunt");
+		String fd_amount=request.getParameter("fd_amount");
 		String fd_type=request.getParameter("fd_type");
 		String stock_type=request.getParameter("stock_type");
 		String mf_type=request.getParameter("mf_type");
 		String bonds_type=request.getParameter("bonds_type");
-		String fd_id=request.getParameter("break_fd");
+		String fd_string=(request.getParameter("break_fd"));
 		String classify=request.getParameter("classify");
 		String username=(String) session.getAttribute("username");
+		String stock_price=request.getParameter("stock_price");
+		String mf_price=request.getParameter("mf_price");
+		String bonds_price=request.getParameter("bonds_price");
 		try{
 			//get variables price, balance and total cost,trans_no and fd_no
 			double balance=0.0;
 			PreparedStatement p = conn.prepareStatement("select balance from users where userid = ?");
 			p.setString(1,username);
-			System.out.println(username);
+
 			ResultSet rs = p.executeQuery();
 			while(rs.next())balance=rs.getInt(1);
 			int fd_no=0;
@@ -86,12 +92,14 @@ public class NewTransaction extends HttpServlet {
 					stock_company=bonds_company;
 					stock_quantity=bonds_quantity;
 					stock_type=bonds_type;
+					stock_price=bonds_price;
 					val="B";
 				}
 				else if(classify.equals("mf")){
 					stock_company=mf_company;
 					stock_quantity=mf_quantity;
 					stock_type=mf_type;
+					stock_price=mf_price;
 					val="MF";
 				}
 				double price=0.0;
@@ -99,26 +107,65 @@ public class NewTransaction extends HttpServlet {
 				p.setString(1,stock_company);
 				rs = p.executeQuery();
 				while(rs.next())price=rs.getDouble(1);
-				int quantity=Integer.parseInt(stock_quantity);
+				int quantity=0;
+				double user_price=0.0;
+				if(stock_quantity.equals(""))
+				{
+					response.sendRedirect("Transact.jsp");
+					return;
+				}
+				if(stock_price.equals("") && (stock_type.endsWith("offer")))
+				{
+					response.sendRedirect("Transact.jsp");
+					return;
+				}
+				if(stock_type.endsWith("offer"))
+				{
+					user_price=Double.parseDouble(stock_price);
+				}
+				quantity=Integer.parseInt(stock_quantity);
 				//			System.out.println("stock was clicked"); debugging
 				double total_cost=price*quantity;
-				System.out.println("Can't proceed further"+ total_cost);
-				if(stock_type.equals("buy"))
+				double user_cost=user_price*quantity;
+				if(stock_type.equals("buy") || stock_type.equals("buy_offer"))
 				{
-					if(balance >= total_cost){
+					if( balance >= user_cost && stock_type.equals("buy_offer") &&  user_price < price )
+					{
+						//remove balance
+						p = conn.prepareStatement("update users set balance = balance - ? where userid = ?");
+						p.setDouble(1, user_cost);
+						p.setString(2, username);
+						p.executeUpdate();
+
+						//add transaction
+						p = conn.prepareStatement("insert into curr_transact values(?,?,?,?,'B',?,?,?)");
+						p.setInt(1, trans_no);
+						p.setString(2,username);
+						p.setString(3, stock_company);
+						p.setString(4, val);
+						p.setDouble(5,user_cost);
+						p.setInt(6,quantity);
+						java.util.Date date= new java.util.Date();
+						Timestamp temp= new Timestamp(date.getTime());
+						p.setTimestamp(7, temp);
+						p.executeUpdate();
+						response.sendRedirect("Transact.jsp?res=trans_buy");
+						
+					}
+					else if(balance >= total_cost){
 						//Update user balance
-						System.out.println("you can proceed" + balance + " - " + total_cost + "reduced from balance");
 						p = conn.prepareStatement("update users set balance = balance - ? where userid = ?");
 						p.setDouble(1, total_cost);
 						p.setString(2, username);
 						p.executeUpdate();
-						System.out.println("Clear tiill here 1");
+
 						//Update user stocks
-						p = conn.prepareStatement("SELECT userid FROM ownership WHERE userid = ? AND ticker_symbol=? AND invest_type='S'");
+						p = conn.prepareStatement("SELECT userid FROM ownership WHERE userid = ? AND ticker_symbol=? AND invest_type=?");
 						p.setString(1, username);
 						p.setString(2, stock_company);
+						p.setString(3, val);
 						rs=p.executeQuery();
-						
+
 						//Update Transaction History
 						p = conn.prepareStatement("INSERT INTO transact_history values(?,?,?,?,'B',?,?,?,'Y')");
 						p.setInt(1, trans_no);
@@ -131,29 +178,31 @@ public class NewTransaction extends HttpServlet {
 						Timestamp temp= new Timestamp(date.getTime());
 						p.setTimestamp(7, temp);
 						p.executeUpdate();
-						
-						
-						System.out.println("Clear till here 2");
+
+
+
 						if(!rs.next())
 						{
-							System.out.println("Clear tiill here 3");
-							p = conn.prepareStatement("INSERT INTO ownership values(?,?,'S',?)");
+
+							p = conn.prepareStatement("INSERT INTO ownership values(?,?,?,?)");
 							p.setString(1, username);
 							p.setString(2, stock_company);
-							p.setInt(3, quantity);
+							p.setInt(4, quantity);
+							p.setString(3,val );
 							p.executeUpdate();
-							System.out.println("Clear tiill here 3");
+
 						}
 						else{
-							System.out.println("Clear tiill here 4");
-							p = conn.prepareStatement("UPDATE ownership SET quantity = quantity + ? WHERE userid = ? AND ticker_symbol = ? AND invest_type='S'");
+
+							p = conn.prepareStatement("UPDATE ownership SET quantity = quantity + ? WHERE userid = ? AND ticker_symbol = ? AND invest_type= ? ");
 							p.setInt(1,quantity);
 							p.setString(2, username);
 							p.setString(3, stock_company );
+							p.setString(4,val);
 							p.executeUpdate();
-							System.out.println("Clear tiill here 4");
+
 						}
-						response.sendRedirect("OrderBook.jsp");
+						response.sendRedirect("Transact.jsp?res=stock_buy");
 					}
 					else{
 						//Insufficient Funds
@@ -162,48 +211,90 @@ public class NewTransaction extends HttpServlet {
 				}
 				else// selling stocks
 				{
-					p = conn.prepareStatement("SELECT quantity FROM ownership WHERE userid = ? AND ticker_symbol = ? AND invest_type='S'");
+					p = conn.prepareStatement("SELECT quantity FROM ownership WHERE userid = ? AND ticker_symbol = ? AND invest_type=?");
 					p.setString(1,username);
 					p.setString(2, stock_company);
+					p.setString(3, val);
 					rs=p.executeQuery();
 					boolean check=rs.next();
-					int owned_quantity=rs.getInt(1);//denotes the owned shares
+					int owned_quantity=0;
+					if(check)owned_quantity = rs.getInt(1);//denotes the owned shares
 					check = check && (owned_quantity >= quantity);
 					if(check)
 					{
-						//Update user balance
-						p = conn.prepareStatement("update users set balance = balance + ? where userid = ?");
-						p.setDouble(1, total_cost);
-						p.setString(2, username);
-						p.executeUpdate();
-						//Update transaction History
-						p = conn.prepareStatement("INSERT INTO transact_history values(?,?,?,?,'S',?,?,?,'Y')");
-						p.setInt(1, trans_no);
-						p.setString(2, username);
-						p.setString(3, stock_company);
-						p.setString(4,val);
-						p.setDouble(5,price);
-						p.setInt(6,quantity);
-						java.util.Date date= new java.util.Date();
-						Timestamp temp= new Timestamp(date.getTime());
-						p.setTimestamp(7, temp);
-						p.executeUpdate();
-						
-						if(owned_quantity == quantity)
+						if(stock_type.equals("sell_offer") && user_price > price)
 						{
-							p = conn.prepareStatement("DELETE FROM ownership WHERE userid = ? AND ticker_symbol = ? AND invest_type='S'");
-							p.setString(1,username);
-							p.setString(2, stock_company);
+							//remove items
+							if(owned_quantity == quantity)
+							{
+								p = conn.prepareStatement("DELETE FROM ownership WHERE userid = ? AND ticker_symbol = ? AND invest_type=?");
+								p.setString(1,username);
+								p.setString(2, stock_company);
+								p.setString(3, val);
+								p.executeUpdate();
+							}
+							else{
+								p = conn.prepareStatement("UPDATE ownership SET quantity = quantity - ? WHERE userid = ? AND ticker_symbol = ? AND invest_type= ?");
+								p.setInt(1,quantity);
+								p.setString(2, username);
+								p.setString(3, stock_company );
+								p.setString(4, val);
+								p.executeUpdate();
+							}
+							
+							//add transaction
+							p = conn.prepareStatement("insert into curr_transact values(?,?,?,?,'S',?,?,?)");
+							p.setInt(1, trans_no);
+							p.setString(2,username);
+							p.setString(3, stock_company);
+							p.setString(4, val);
+							p.setDouble(6,user_cost);
+							p.setInt(7,quantity);
+							java.util.Date date= new java.util.Date();
+							Timestamp temp= new Timestamp(date.getTime());
+							p.setTimestamp(8, temp);
 							p.executeUpdate();
+							
+							response.sendRedirect("Transact.jsp?res=trans_sell");
 						}
-						else{
-							p = conn.prepareStatement("UPDATE ownership SET quantity = quantity - ? WHERE userid = ? AND ticker_symbol = ? AND invest_type='S'");
-							p.setInt(1,quantity);
+						else
+						{
+							//Update user balance
+							p = conn.prepareStatement("update users set balance = balance + ? where userid = ?");
+							p.setDouble(1, total_cost);
 							p.setString(2, username);
-							p.setString(3, stock_company );
 							p.executeUpdate();
+							//Update transaction History
+							p = conn.prepareStatement("INSERT INTO transact_history values(?,?,?,?,'S',?,?,?,'Y')");
+							p.setInt(1, trans_no);
+							p.setString(2, username);
+							p.setString(3, stock_company);
+							p.setString(4,val);
+							p.setDouble(5,price);
+							p.setInt(6,quantity);
+							java.util.Date date= new java.util.Date();
+							Timestamp temp= new Timestamp(date.getTime());
+							p.setTimestamp(7, temp);
+							p.executeUpdate();
+
+							if(owned_quantity == quantity)
+							{
+								p = conn.prepareStatement("DELETE FROM ownership WHERE userid = ? AND ticker_symbol = ? AND invest_type=?");
+								p.setString(1,username);
+								p.setString(2, stock_company);
+								p.setString(3, val);
+								p.executeUpdate();
+							}
+							else{
+								p = conn.prepareStatement("UPDATE ownership SET quantity = quantity - ? WHERE userid = ? AND ticker_symbol = ? AND invest_type= ?");
+								p.setInt(1,quantity);
+								p.setString(2, username);
+								p.setString(3, stock_company );
+								p.setString(4, val);
+								p.executeUpdate();
+							}
+							response.sendRedirect("Transact.jsp?res=stock_sell");
 						}
-						response.sendRedirect("OrderBook.jsp?");
 					}
 					else{
 						//Insufficient items to sell
@@ -212,13 +303,124 @@ public class NewTransaction extends HttpServlet {
 				}
 				p = conn.prepareStatement("update game_params set value = ? where game_param = 'trans_no'");
 				trans_no++;
-				System.out.println(trans_no);
+
 				p.setString(1, String.valueOf(trans_no));
 				p.executeUpdate();
 			}
 			else if(classify.equals("fd"))
 			{
-				//			System.out.println("fd was clicked");debugging
+
+				if(fd_type.equals("Buy_FD"))
+				{
+					double interest=0.0;
+					p = conn.prepareStatement("select interest_rate from fd_rates where duration = ?");
+					PGInterval d= new PGInterval(fd_period);
+					p.setObject(1,d);
+
+					rs = p.executeQuery();
+					while(rs.next())interest=rs.getDouble(1);
+
+					double amount=0.0;
+					if(fd_amount.equals(""))
+					{
+						response.sendRedirect("Transact.jsp");
+						return;
+					}
+					amount=Double.parseDouble(fd_amount);
+					if(amount > balance)
+					{
+						response.sendRedirect("Transact.jsp?res=funds");
+					}
+					else
+					{
+						//remove money
+						p = conn.prepareStatement("update users set balance = balance - ? where userid = ?");
+						p.setDouble(1, amount);
+						p.setString(2, username);
+						p.executeUpdate();
+
+
+						//Insert into fd
+						p = conn.prepareStatement("INSERT INTO fd_table values(?,?,?,?,?,?)");
+						p.setInt(1, fd_no);
+						p.setString(2, username);
+						p.setDouble(3,amount);
+						p.setDouble(5,interest);
+						Date utilDate = new Date();
+						java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
+						p.setDate(4,sqlDate);
+						p.setObject(6, d);
+						p.executeUpdate();
+						response.sendRedirect("Transact.jsp?res=fd_buy");
+						p = conn.prepareStatement("update game_params set value = ? where game_param = 'fd_no'");
+						fd_no++;
+
+						p.setString(1, String.valueOf(fd_no));
+						p.executeUpdate();
+					}
+				}
+				else
+				{
+					int fd_id=0;
+					if(fd_string.equals(""))
+					{
+						response.sendRedirect("Transact.jsp");
+						return;
+					}
+					fd_id=Integer.parseInt(fd_string);
+					p = conn.prepareStatement("SELECT * from fd_table where fd_id= ? and userid = ?");
+					p.setInt(1,fd_id);
+					p.setString(2, username);
+					rs=p.executeQuery();
+					if(rs.next())
+					{
+						Double amount=rs.getDouble(3);
+						Double ini_amount=amount;
+						Double rate=rs.getDouble(5);
+						java.sql.Date d = rs.getDate(4);
+						PGInterval dur=(PGInterval) rs.getObject(6);
+						p = conn.prepareStatement("SELECT * from fd_table where fd_id= ? AND day_of_issue + fd_duration > ?");
+						p.setInt(1, fd_id);
+						Date utilDate = new Date();
+						java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
+						p.setDate(2,sqlDate);
+						rs=p.executeQuery();
+						String suc="N";
+						if(rs.next())
+						{
+							int months = dur.getMonths()*12 + dur.getYears(); 
+							amount=amount+amount*months*rate/1200;
+							suc="Y";
+						}
+						//remove from fd_table
+						p = conn.prepareStatement("delete from fd_table where fd_id= ?");
+						p.setInt(1,fd_id);
+						p.executeUpdate();
+						//Add to fd_history
+						p = conn.prepareStatement("insert into fd_history values(?,?,?,?,?,?,?)");
+						p.setInt(1,fd_id);
+						p.setString(2,username);
+						p.setDouble(3,ini_amount);
+						p.setDate(4,d);
+						p.setDouble(5, rate);
+						p.setObject(6,dur);
+						p.setString(7, suc);
+						p.executeUpdate();
+						//give money to users
+						p = conn.prepareStatement("update users set balance = balance + ? where userid = ?");
+						p.setDouble(1, amount);
+						p.setString(2, username);
+						p.executeUpdate();
+						response.sendRedirect("Transact.jsp?res=fd_broken");
+					}
+					else
+					{
+						//Incorrect fd_id
+						response.sendRedirect("Transact.jsp?res=invalid_id");
+					}
+				}
 			}
 		}catch (SQLException e) {
 			// TODO Auto-generated catch block
